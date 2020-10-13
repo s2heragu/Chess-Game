f269c9ea050097c29e4534c6b31a6a1eb8d2676d
s2heragu <s2heragu@gmail.com>
2020-10-13 12:24:03 -0400

    added chesspiece class

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -4,6 +4,7 @@ public class Chess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ChessPiece pawn = new ChessPiece();
 		System.out.println("Hello Chess");
 	}
 
