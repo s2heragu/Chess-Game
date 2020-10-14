403780ca6bd2520b6f286a8b763681fb197a4bc7
Jonathan Wong <jrwongacademy@gmail.com>
2020-10-14 15:14:58 -0400

    generated Initial Javadocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"pieces","l":"Bishop"},{"p":"chess","l":"Board"},{"p":"chess","l":"Chess"},{"p":"pieces","l":"ChessPiece"},{"p":"pieces","l":"King"},{"p":"pieces","l":"Knight"},{"p":"pieces","l":"Pawn"},{"p":"pieces","l":"Queen"},{"p":"pieces","l":"Rook"}]
--- src/pieces/ChessPiece.java
+++ src/pieces/ChessPiece.java
@@ -1,47 +1,88 @@
 package pieces;
 
 import chess.Board;
-
+/**
+ * 
+ * 
+ * @author Jonathan Wong, Shreyas Heragu
+ * Abstract class to fulfill role of different chess pieces.
+ *
+ */
 public abstract class ChessPiece {
+	/**
+	 * Keeps track of piece's row
+	 */
 	private int row;
+	/**
+	 * Keeps track of piece's column
+	 */
 	private int col;
+	/**
+	 * Keeps track of piece's team color
+	 */
 	private boolean isWhite;
+	/**
+	 * Keeps track of piece's type (King, Queen, Bishop, Knight, Rook, Pawn)
+	 */
 	private char type;
-	
+	/**
+	 * Constructor to initialize row, column and team
+	 * @param row	the row of the piece
+	 * @param col	the column of the piece
+	 * @param isWhite	the piece's team
+	 */
 	public ChessPiece(int row, int col, boolean isWhite) {
 		this.isWhite = isWhite;
 		this.row = row;
 		this.col = col;
 	}
 	
+	/**
+	 * Gets row
+	 * @return this.row the piece's row
+	 */
 	public int row() {
 		return this.row;
 	}
-	
+	/**
+	 * Gets column
+	 */
 	public int col() {
 		return this.col;
 	}
-	
+	/**
+	 * Gets team
+	 */
 	public boolean isWhite() {
 		return this.isWhite;
 	}
-	
+	/**
+	 * Gets piece type
+	 */
 	public char type() {
 		return this.type;
 	}
-	
+	/**
+	 * Sets row
+	 */
 	public void setRow(int row) {
 		this.row = row;
 	}
-	
+	/**
+	 * Sets column
+	 */
 	public void setCol(int col) {
 		this.col = col;
 	}
-	
+	/**
+	 * Sets type
+	 */
 	protected void setType(char type) {
 		this.type = type;
 	}
-	
+	/**
+	 * Sets row
+	 */
 	protected boolean errorCheck(int newRow, int newCol) {
 		if(newRow > 7 || newCol > 7 || newRow < 0 || newCol < 0) {
 			return true;
