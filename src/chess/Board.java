ca8bc4a4e9429d35d3f1ec4fecb7e102c3d0a78e
Jonathan Wong <jrwongacademy@gmail.com>
2020-10-16 17:27:47 -0400

    Check for king checked and checkmate

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -54,53 +54,321 @@ public class Board{
 	}
 	
 	/**
-	 * Checks if King on opposing team got checked upon most recent move
-	 * @return result Boolean to determine whether King is checked
+	 * 
+	 * @param row	destination row
+	 * @param col	destination column
+	 * @param piece	Chess Piece to be moved
 	 */
-	public boolean kingChecked() {
-		boolean result = false;
+	public void movePieceNoCheck(int row, int col, ChessPiece piece) {
+		board[row][col] = piece;
+		piece.setRow(row);
+		piece.setCol(col);
+	}
+	
+	/**
+	 * Determines whether game is in check mate
+	 * @return result boolean that is true if check mated, false otherwise
+	 */
+	public boolean isCheckMate(boolean kingTeam) {
+		boolean result = true;
 		//Dummy values
 		King k = new King(-1, -1, true);
+		//Find the king
 		for(int i = 1; i <= 8; i++) {
 			for(int j = 1; j <= 8; j++) {
 				if(board[i][j].type() == 'K') {
-					
-					//White makes move that checks black King
-					if(isWhiteTurn && !board[i][j].isWhite()) {
+					if(board[i][j].isWhite() == kingTeam) {
 						k = (King) board[i][j];
 					}
-					//Black makes move that checks white king
-					//Change turn after this check, this can be changed
-					else if (!isWhiteTurn && board[i][j].isWhite()) {
-						k = (King) board[i][j];
+				}
+			}
+		}
+		Board tempBoard = copy();
+		
+		int kingRow = k.row();
+		int kingCol = k.col();
+		
+		int row = kingRow;
+		int col = kingCol;
+		
+		if(!kingChecked(kingTeam)) {
+			return false;
+		}
+		
+		row = kingRow;
+		col = kingCol;
+		row++;
+		col--;
+		//Valid position
+		if(!(row < 1 || col >= 8)) {
+			if(!(row < 1 || col >= 8)) {
+				//Other spot has to be free to move to
+				if(board[row][col] == null) {
+					//This spot has to be attacked by other team
+					if(!isSpotAttacked(row, col, !kingTeam)) {
+						return false;
 					}
-					
+					//Can we defend it and no longer be in check
+					else {
+						if(isSpotAttacked(row, col, kingTeam)) {
+							ChessPiece defender = tempBoard.spotAttacker(row, col, kingTeam);
+							tempBoard.movePieceNoCheck(row, col, defender);
+							//Not checked after moving
+							if(!tempBoard.kingChecked(kingTeam)) {
+								return false;
+							}
+						}
+					}
+				}
+			}
+		}
+		tempBoard = copy();
+
+		row = kingRow;
+		col = kingCol;
+		row++;
+		//Valid position
+		if(!(row < 1 || col >= 8)) {
+			if(!(row < 1 || col >= 8)) {
+				//Other spot has to be free to move to
+				if(board[row][col] == null) {
+					//This spot has to be attacked by other team
+					if(!isSpotAttacked(row, col, !kingTeam)) {
+						return false;
+					}
+				}
+				//Can we defend it and no longer be in check
+				else {
+					if(isSpotAttacked(row, col, kingTeam)) {
+						ChessPiece defender = tempBoard.spotAttacker(row, col, kingTeam);
+						tempBoard.movePieceNoCheck(row, col, defender);
+						//Not checked after moving
+						if(!tempBoard.kingChecked(kingTeam)) {
+							return false;
+						}
+					}
 				}
 			}
 		}
 		
-		//Check diagonals
-		int row = k.row();
-		int col = k.col();
+		
+		tempBoard = copy();
+		row = kingRow;
+		col = kingCol;
+		row++;
+		col++;
+		//Valid position
+		if(!(row < 1 || col >= 8)) {
+			if(!(row < 1 || col >= 8)) {
+				//Other spot has to be free to move to
+				if(board[row][col] == null) {
+					//This spot has to be attacked by other team
+					if(!isSpotAttacked(row, col, !kingTeam)) {
+						return false;
+					}
+				}
+				//Can we defend it and no longer be in check
+				else {
+					if(isSpotAttacked(row, col, kingTeam)) {
+						ChessPiece defender = tempBoard.spotAttacker(row, col, kingTeam);
+						tempBoard.movePieceNoCheck(row, col, defender);
+						//Not checked after moving
+						if(!tempBoard.kingChecked(kingTeam)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		tempBoard = copy();
+		row = kingRow;
+		col = kingCol;
+		col++;
+		//Valid position
+		if(!(row < 1 || col >= 8)) {
+			if(!(row < 1 || col >= 8)) {
+				//Other spot has to be free to move to
+				if(board[row][col] == null) {
+					//This spot has to be attacked by other team
+					if(!isSpotAttacked(row, col, !kingTeam)) {
+						return false;
+					}
+				}
+				//Can we defend it and no longer be in check
+				else {
+					if(isSpotAttacked(row, col, kingTeam)) {
+						ChessPiece defender = tempBoard.spotAttacker(row, col, kingTeam);
+						tempBoard.movePieceNoCheck(row, col, defender);
+						//Not checked after moving
+						if(!tempBoard.kingChecked(kingTeam)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		tempBoard = copy();
+		row = kingRow;
+		col = kingCol;
+		row--;
+		col++;
+		//Valid position
+		if(!(row < 1 || col >= 8)) {
+			if(!(row < 1 || col >= 8)) {
+				//Other spot has to be free to move to
+				if(board[row][col] == null) {
+					//This spot has to be attacked by other team
+					if(!isSpotAttacked(row, col, !kingTeam)) {
+						return false;
+					}
+				}
+				//Can we defend it and no longer be in check
+				else {
+					if(isSpotAttacked(row, col, kingTeam)) {
+						ChessPiece defender = tempBoard.spotAttacker(row, col, kingTeam);
+						tempBoard.movePieceNoCheck(row, col, defender);
+						//Not checked after moving
+						if(!tempBoard.kingChecked(kingTeam)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		tempBoard = copy();
+		row = kingRow;
+		col = kingCol;
+		col--;
+		//Valid position
+		if(!(row < 1 || col >= 8)) {
+			if(!(row < 1 || col >= 8)) {
+				//Other spot has to be free to move to
+				if(board[row][col] == null) {
+					//This spot has to be attacked by other team
+					if(!isSpotAttacked(row, col, !kingTeam)) {
+						return false;
+					}
+				}
+				//Can we defend it and no longer be in check
+				else {
+					if(isSpotAttacked(row, col, kingTeam)) {
+						ChessPiece defender = tempBoard.spotAttacker(row, col, kingTeam);
+						tempBoard.movePieceNoCheck(row, col, defender);
+						//Not checked after moving
+						if(!tempBoard.kingChecked(kingTeam)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		tempBoard = copy();
+		row = kingRow;
+		col = kingCol;
+		row--;
+		col--;
+		//Valid position
+		if(!(row < 1 || col >= 8)) {
+			if(!(row < 1 || col >= 8)) {
+				//Other spot has to be free to move to
+				if(board[row][col] == null) {
+					//This spot has to be attacked by other team
+					if(!isSpotAttacked(row, col, !kingTeam)) {
+						return false;
+					}
+				}
+				//Can we defend it and no longer be in check
+				else {
+					if(isSpotAttacked(row, col, kingTeam)) {
+						ChessPiece defender = tempBoard.spotAttacker(row, col, kingTeam);
+						tempBoard.movePieceNoCheck(row, col, defender);
+						//Not checked after moving
+						if(!tempBoard.kingChecked(kingTeam)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		tempBoard = copy();
+		row = kingRow;
+		col = kingCol;
+		col--;
+		//Valid position
+		if(!(row < 1 || col >= 8)) {
+			if(!(row < 1 || col >= 8)) {
+				//Other spot has to be free to move to
+				if(board[row][col] == null) {
+					//This spot has to be attacked by other team
+					if(!isSpotAttacked(row, col, !kingTeam)) {
+						return false;
+					}
+				}
+				//Can we defend it and no longer be in check
+				else {
+					if(isSpotAttacked(row, col, kingTeam)) {
+						ChessPiece defender = tempBoard.spotAttacker(row, col, kingTeam);
+						tempBoard.movePieceNoCheck(row, col, defender);
+						//Not checked after moving
+						if(!tempBoard.kingChecked(kingTeam)) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		
+		
+		
+		
+		return result;
+	}
+	
+	
+	/**
+	 * 
+	 * @param attackRow	the row being attacked
+	 * @param attackCol the column being attacked
+	 * @param isWhiteTeam the team doing the attacking, true if white and false for black team
+	 * @return		true if spot is attacked by the team and false otherwise
+	 */
+	public boolean isSpotAttacked(int attackRow,int attackCol, boolean isWhiteTeam) {
+		if(spotAttacker(attackRow, attackCol, isWhiteTeam) != null) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * 
+	 * @param row the row that is being attacked
+	 * @param col the column being attacked
+	 * @param isWhiteTeam the team doing the attacking, true if white and false for black team
+	 * @return 		the attacking piece, null if not present
+	 */
+	public ChessPiece spotAttacker(int attackRow, int attackCol, boolean isWhiteTeam) {
+		int row = attackRow;
+		int col = attackCol;
 		int distance = 0;
+		//Diagonals
+		
+		
+		//Down and left
 		row--;
 		col--;
 		distance++;
-		boolean team = k.isWhite();
-		//Down and left
 		while(row > 0 && col > 0) {
 			if(board[row][col] != null) {
 				ChessPiece other = board[row][col];
-				if(other.isWhite() != team) {
-					//Diagonals attacked by bishop, pawn, Queen
-					if(distance == 1 && other.type() == 'p' && other.isWhite()) {
-						return true;
+				if(other.isWhite() == isWhiteTeam) {
+					//Only white pawn can move and up right to attack
+					if(other.type() == 'p' && distance == 1 && other.isWhite()) {
+						return other;
 					}
 					else if(other.type() == 'B' || other.type() == 'Q') {
-						return true;
+						return other;
 					}
 				}
-				//Same team, not attacked diagonally
+				//Wrong team, done checking
 				else {
 					break;
 				}
@@ -110,26 +378,25 @@ public class Board{
 			distance++;
 		}
 		//Down and right
-		row = k.row();
-		col = k.col();
+		row = attackRow;
+		col = attackCol;
 		distance = 0;
 		row--;
 		col++;
 		distance++;
-		//Down and left
 		while(row > 0 && col <= 8) {
 			if(board[row][col] != null) {
 				ChessPiece other = board[row][col];
-				if(other.isWhite() != team) {
-					//Diagonals attacked by bishop, pawn, Queen
-					if(distance == 1 && other.type() == 'p' && other.isWhite()) {
-						return true;
+				if(other.isWhite() == isWhiteTeam) {
+					//Only white pawn can move and up right to attack
+					if(other.type() == 'p' && distance == 1 && other.isWhite()) {
+						return other;
 					}
 					else if(other.type() == 'B' || other.type() == 'Q') {
-						return true;
+						return other;
 					}
 				}
-				//Same team, not attacked diagonally
+				//Wrong team, done checking
 				else {
 					break;
 				}
@@ -138,37 +405,325 @@ public class Board{
 			col++;
 			distance++;
 		}
-		//Up and left
-		row = k.row();
-		col = k.col();
+		
+		
+		//Up and Left
+		row = attackRow;
+		col = attackCol;
 		distance = 0;
-		row--;
-		col++;
+		row++;
+		col--;
 		distance++;
-		//Down and left
-		while(row > 0 && col <= 8) {
+		while(row <= 8 && col > 0) {
 			if(board[row][col] != null) {
 				ChessPiece other = board[row][col];
-				if(other.isWhite() != team) {
-					//Diagonals attacked by bishop, pawn, Queen
-					if(distance == 1 && other.type() == 'p' && other.isWhite()) {
-						return true;
+				if(other.isWhite() == isWhiteTeam) {
+					//Only black pawn can move diagonally down to attack;
+					if(other.type() == 'p' && distance == 1 && (!other.isWhite())) {
+						return other;
 					}
 					else if(other.type() == 'B' || other.type() == 'Q') {
-						return true;
+						return other;
 					}
 				}
-				//Same team, not attacked diagonally
+				//Wrong team, done checking
+				else {
+					break;
+				}
+			}
+			row++;
+			col--;
+			distance++;
+		}
+		//Up and right
+		row = attackRow;
+		col = attackCol;
+		distance = 0;
+		row++;
+		col++;
+		distance++;
+		while(row <= 8 && col <= 8) {
+			if(board[row][col] != null) {
+				ChessPiece other = board[row][col];
+				if(other.isWhite() == isWhiteTeam) {
+					//Only black pawn can move diagonally down to attack;
+					if(other.type() == 'p' && distance == 1 && (!other.isWhite())) {
+						return other;
+					}
+					else if(other.type() == 'B' || other.type() == 'Q') {
+						return other;
+					}
+				}
+				//Wrong team, done checking
+				else {
+					break;
+				}
+			}
+			row++;
+			col++;
+			distance++;
+		}
+		
+		distance = 0;
+		//Horizontal, vertical
+		
+		//Left
+		row = attackRow;
+		col = attackCol;
+		col--;
+		while(col > 0) {
+			if(board[row][col] != null) {
+				ChessPiece other = board[row][col];
+				if(other.isWhite() == isWhiteTeam) {
+					//Only rook and queen can attack
+					if(other.type() == 'R' || other.type() == 'Q') {
+						return other;
+					}
+				}
+				//Wrong team, done checking
+				else {
+					break;
+				}
+			}
+			col--;
+		}
+		
+		//Left
+		row = attackRow;
+		col = attackCol;
+		col++;
+		while(col > 0) {
+			if(board[row][col] != null) {
+				ChessPiece other = board[row][col];
+				if(other.isWhite() == isWhiteTeam) {
+					//Only rook and queen can attack
+					if(other.type() == 'R' || other.type() == 'Q') {
+						return other;
+					}
+				}
+				//Wrong team, done checking
+				else {
+					break;
+				}
+			}
+			col++;
+		}
+		
+		//down
+		row = attackRow;
+		col = attackCol;
+		row--;
+		while(col > 0) {
+			if(board[row][col] != null) {
+				ChessPiece other = board[row][col];
+				if(other.isWhite() == isWhiteTeam) {
+					//Only rook and queen can attack
+					if(other.type() == 'R' || other.type() == 'Q') {
+						return other;
+					}
+				}
+				//Wrong team, done checking
 				else {
 					break;
 				}
 			}
 			row--;
-			col--;
-			distance++;
 		}
-		//Up and right
 		
+		//up
+		row = attackRow;
+		col = attackCol;
+		row++;
+		while(col > 0) {
+			if(board[row][col] != null) {
+				ChessPiece other = board[row][col];
+				if(other.isWhite() == isWhiteTeam) {
+					//Only rook and queen can attack
+					if(other.type() == 'R' || other.type() == 'Q') {
+						return other;
+					}
+				}
+				//Wrong team, done checking
+				else {
+					break;
+				}
+			}
+			row++;
+		}
+		
+		
+		//Knights
+		row = attackRow;
+		col = attackCol;
+		int knightRow = row + 1;
+		int knightCol = col - 2;
+		//Check knights
+		//Check valid position
+		if(!(knightRow < 1 || knightRow >= 8)) {
+			if(!(knightCol < 1 || knightCol >= 8)) {
+				if(board[knightRow][knightCol] != null) {
+					ChessPiece other = board[row][col];
+					//Knight of attacking team
+					if(other.type() == 'N' && (other.isWhite() == isWhiteTeam)) {
+						return other;
+					}
+				}
+			}
+		}
+		
+		//Knights
+		row = attackRow;
+		col = attackCol;
+		knightRow = row + 2;
+		knightCol = col - 1;
+		//Check knights
+		//Check valid position
+		if(!(knightRow < 1 || knightRow >= 8)) {
+			if(!(knightCol < 1 || knightCol >= 8)) {
+				if(board[knightRow][knightCol] != null) {
+					ChessPiece other = board[row][col];
+					//Knight of attacking team
+					if(other.type() == 'N' && (other.isWhite() == isWhiteTeam)) {
+						return other;
+					}
+				}
+			}
+		}
+		row = attackRow;
+		col = attackCol;
+		knightRow = row + 2;
+		knightCol = col + 1;
+		//Check knights
+		//Check valid position
+		if(!(knightRow < 1 || knightRow >= 8)) {
+			if(!(knightCol < 1 || knightCol >= 8)) {
+				if(board[knightRow][knightCol] != null) {
+					ChessPiece other = board[row][col];
+					//Knight of attacking team
+					if(other.type() == 'N' && (other.isWhite() == isWhiteTeam)) {
+						return other;
+					}
+				}
+			}
+		}
+		row = attackRow;
+		col = attackCol;
+		knightRow = row + 1;
+		knightCol = col + 2;
+		//Check knights
+		//Check valid position
+		if(!(knightRow < 1 || knightRow >= 8)) {
+			if(!(knightCol < 1 || knightCol >= 8)) {
+				if(board[knightRow][knightCol] != null) {
+					ChessPiece other = board[row][col];
+					//Knight of attacking team
+					if(other.type() == 'N' && (other.isWhite() == isWhiteTeam)) {
+						return other;
+					}
+				}
+			}
+		}
+		
+		row = attackRow;
+		col = attackCol;
+		knightRow = row - 1;
+		knightCol = col + 2;
+		//Check knights
+		//Check valid position
+		if(!(knightRow < 1 || knightRow >= 8)) {
+			if(!(knightCol < 1 || knightCol >= 8)) {
+				if(board[knightRow][knightCol] != null) {
+					ChessPiece other = board[row][col];
+					//Knight of attacking team
+					if(other.type() == 'N' && (other.isWhite() == isWhiteTeam)) {
+						return other;
+					}
+				}
+			}
+		}
+		row = attackRow;
+		col = attackCol;
+		knightRow = row - 2;
+		knightCol = col + 1;
+		//Check knights
+		//Check valid position
+		if(!(knightRow < 1 || knightRow >= 8)) {
+			if(!(knightCol < 1 || knightCol >= 8)) {
+				if(board[knightRow][knightCol] != null) {
+					ChessPiece other = board[row][col];
+					//Knight of attacking team
+					if(other.type() == 'N' && (other.isWhite() == isWhiteTeam)) {
+						return other;
+					}
+				}
+			}
+		}
+		row = attackRow;
+		col = attackCol;
+		knightRow = row - 2;
+		knightCol = col - 1;
+		//Check knights
+		//Check valid position
+		if(!(knightRow < 1 || knightRow >= 8)) {
+			if(!(knightCol < 1 || knightCol >= 8)) {
+				if(board[knightRow][knightCol] != null) {
+					ChessPiece other = board[row][col];
+					//Knight of attacking team
+					if(other.type() == 'N' && (other.isWhite() == isWhiteTeam)) {
+						return other;
+					}
+				}
+			}
+		}
+		row = attackRow;
+		col = attackCol;
+		knightRow = row - 1;
+		knightCol = col - 2;
+		//Check knights
+		//Check valid position
+		if(!(knightRow < 1 || knightRow >= 8)) {
+			if(!(knightCol < 1 || knightCol >= 8)) {
+				if(board[knightRow][knightCol] != null) {
+					ChessPiece other = board[row][col];
+					//Knight of attacking team
+					if(other.type() == 'N' && (other.isWhite() == isWhiteTeam)) {
+						return other;
+					}
+				}
+			}
+		}
+		
+		
+		//No piece attacking
+		return null;
+	}
+	
+	/**
+	 * Checks if King on opposing team got checked upon most recent move
+	 * @param kingTeam checks if this king's team is checked
+	 * @return result Boolean to determine whether King is checked
+	 */
+	public boolean kingChecked(boolean kingTeam) {
+		boolean result = false;
+		//Dummy values
+		King k = new King(-1, -1, true);
+		//Find the king
+		for(int i = 1; i <= 8; i++) {
+			for(int j = 1; j <= 8; j++) {
+				if(board[i][j].type() == 'K') {
+					if(board[i][j].isWhite() == kingTeam) {
+						k = (King) board[i][j];
+					}
+				}
+			}
+		}
+		
+		int kingRow = k.row();
+		int kingCol = k.col();
+		//Being attacked by opposing team
+		if(isSpotAttacked(kingRow, kingCol, !k.isWhite())) {
+			return true;
+		}
 		
 		
 		return result;
