e38478a4c8b5ec2636ea7697d0b0678189f0d68a
Jonathan Wong <jrwongacademy@gmail.com>
2020-10-23 12:25:08 -0400

    added package info for javadocs

---
+++ src/chess/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides classes to run a 2 player chess game.
+ * The board class to represent the board is provided and perform and check moves.
+ * Chess.java is provided to drive the game.
+ */
+package chess;
---
+++ src/pieces/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes to store different chess piece typed classes.
+ * Includes abstract class ChessPiece.
+ * Includes concrete subclasses of ChessPiece for Rook, Knight, Bishop, Pawn, Queen and King.
+ * Each piece can determine its own move legality.
+ */
+package pieces;
